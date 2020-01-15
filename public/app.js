cd096f5bce8e6585eff54235d45d7d1a941b9dc0
Levar Barrett <emaillevar@gmail.com>
2020-01-15 14:29:39 -0500

    update visual

--- public/app.js
+++ public/app.js
@@ -151,7 +151,7 @@ $(document).ready(function() {
     console.log("Delete note button clicked");
     // var thisID = $(this).attr("article-id");
     $.ajax({
-      type: "POST",
+      te: "POST",
       url: "/deleteNote/" + articleID
     });
     $("#posted-notes").val("");
